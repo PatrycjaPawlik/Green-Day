d55e48fbecee2a25caf1ef3bb731283ce021a23c
Patrycja Pawlik <patrycjapawlik21@gmail.com>
2022-05-25 12:53:52 +0200

    changed js code to three functions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-
-console.log("Hello")
 {
-  const button = document.querySelector(".js-button");
-  
-  button.addEventListener("click", () => {
+  const hello = () => {
+    console.log("Hello");
+  };
+
+  const toggleBackgroundColor = () => {
     const body = document.querySelector(".body");
     const bodyColor = document.querySelector(".js-bodyColor");
+
     body.classList.toggle("white");
     if (body.classList.contains("white")) {
       bodyColor.innerText = "zielone";
     } else {
       bodyColor.innerText = "białe";
     }
-  });
-}
+  }
 
-button();
+  const init = () => {
+    const button = document.querySelector(".js-button");
+    button.addEventListener("click", toggleBackgroundColor);
+  }
+
+  hello ();
+
+  init ();
+}
